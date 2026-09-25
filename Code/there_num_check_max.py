a216794790da2fc6d957c04f7cce4565a3962fa7
rishabh-28-shubham <ectopdash@gmail.com>
2026-09-25 14:52:20 +0530

    hello function implemented

--- Code/there_num_check_max.py
+++ Code/there_num_check_max.py
@@ -1,10 +1,17 @@
+def greet():
+    print("hello")
+
 x = int(input("x : "))
 y = int(input("y: "))
 z = int(input("z: "))
 
+greet()
 if (x>y and x>z):
     print(f" {x}  is largest")
+    greet()
 elif(y>x and y>z):
     print(f" {y} is largest")
+    greet()
 else:
     print(f"{z} is largest")
+    greet()
